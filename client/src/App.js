b0ba0fe41c0dcb9d76654245a3a67659bb907600
Henry Liu <hl3184@columbia.edu>
2020-08-30 01:58:50 -0400

    contact context and global state setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+import ContactState from './context/contact/ContactState';
 import Navbar from './components/layout/Navbar';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
 
 export default () => {
   return (
-    <Router>
-      <Navbar />
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-        </Switch>
-      </div>
-    </Router>
+    <ContactState>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </Router>
+    </ContactState>
   );
 };
