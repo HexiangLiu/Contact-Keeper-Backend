b104e0b21e987e15e484319108c08b3a74bbb997
Henry Liu <hl3184@columbia.edu>
2020-08-30 01:30:36 -0400

    Navbar and Router setup

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Navbar = ({ title, icon }) => {
+  return (
+    <nav className="navbar bg-primary navbar-expand-lg navbar-dark">
+      <a href="/" className="navbar-brand h1">
+        <i className={icon} /> {title}
+      </a>
+      <ul className="navbar-nav ml-auto">
+        <li className="nav-item">
+          <Link className="nav-link" to="/">
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/about">
+            About
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: 'Contact Keeper',
+  icon: 'fas fa-id-card-alt',
+};
+
+export default Navbar;
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="text-center">
+      <h1>About This App</h1>
+      <p className="my-2">
+        This is a full stack React App for keeping contacts
+      </p>
+      <span className="bg-dark text-white p-1">
+        <strong>Version:</strong> 1.0.0
+      </span>
+    </div>
+  );
+};
+
+export default About;
