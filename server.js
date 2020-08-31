261206bcf132b318ee758144318d3457f59e9a93
Henry Liu <hl3184@columbia.edu>
2020-08-31 15:00:27 -0400

    fixed deployment issue

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use('/api/auth', require('./routes/auth'));
 // Serve static assets in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static(__dirname + 'client/build'));
+  app.use(express.static(__dirname + '/client/build'));
 
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
